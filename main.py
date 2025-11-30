5bca7371acf770f6edb75fdb5fd7cb067a891c37
Eli Hause <elijhause@gmail.com>
2025-11-29 20:55:55 -0500

    Add ignore and first real to main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def get_book_text(filepath):
+
+	with open(filepath, encoding="utf-8") as f:
+
+		return f.read()
+
+def main():
+	book_text = get_book_text("books/frankenstein.txt")
+	print(book_text)
+
+if __name__ == "__main__":
+	main()
+
