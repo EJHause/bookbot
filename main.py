fdbc9301f1e124cf4e30c6bca500c2da22869a51
Eli Hause <elijhause@gmail.com>
2025-11-30 20:03:02 -0500

    Text count update

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 def get_book_text(filepath):
+    with open(filepath, encoding="utf-8") as f:
+        return f.read()
 
-	with open(filepath, encoding="utf-8") as f:
-
-		return f.read()
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 def main():
-	book_text = get_book_text("books/frankenstein.txt")
-	print(book_text)
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(book_text)
+    print(f"Found {num_words} total words")
 
 if __name__ == "__main__":
 	main()
