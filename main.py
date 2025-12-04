9fc4702161933be3eba2864d9e1fefefbd9d50a5
Eli Hause <elijhause@gmail.com>
2025-12-03 20:03:04 -0500

    Add clean display with sorting

--- main.py
+++ main.py
@@ -6,14 +6,32 @@ from stats import count_words
 
 from stats import char_count
 
+from stats import sort_char_counts
+
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    filepath = "books/frankenstein.txt"
+    book_text = get_book_text(filepath)
+
+    #Word count
     num_words = count_words(book_text)
-    print(f"Found {num_words} total words")
 
     # Character count
     char_counts = char_count(book_text)
-    print(char_counts)
+
+    #Sorted character counts
+    sorted_counts = sort_char_counts(char_counts)
+
+    #Report output
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath} ...")
+    print("----------- Word Count -----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count ---------")
+
+    for item in sorted_counts:
+          print(f"{item['char']}: {item['num']}")
+
+    print("============= END =============")
 
 if __name__ == "__main__":
 	main()
--- stats.py
+++ stats.py
@@ -14,3 +14,16 @@ def char_count(text):
         else:
             char_counts[char] = 1
     return char_counts
+
+def sort_char_counts(char_counts):
+    #convert dictionary
+    char_list = [{"char": c, "num": n} for c, n in char_counts.items() if c.isalpha()]
+    def get_num(item):
+        return item["num"]
+
+    #sort list in place, greatest to least
+    char_list.sort(key=get_num, reverse=True)
+
+    return char_list
+
+
